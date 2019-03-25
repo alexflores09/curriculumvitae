4b9aef2ddb607b27b39cfb9716f5d062863ee66a
Alexander Flores <aflores.mingob@gmail.com>
2019-03-25 15:54:06 -0600

    Estructura inicial de cv

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const About = () => (
+    <div className="About">
+        <div className="About-container">
+            <div className="About-avatar">
+                <figure>
+                    <img src="/" alt=""/>
+                </figure>
+            </div>
+            <div className="About-name">
+                <h2>Alexander Flores</h2>
+            </div>
+            <div className="About-profession">
+                <p>Mi profesión</p>
+            </div>
+            <div className="About-desc">
+                <p>Soy desarrollador web</p>
+            </div>
+            <div className="About-location">
+                <p>Guatemala</p>
+            </div>
+            <div className="About-social">
+                <p>social</p>
+            </div>
+        </div>
+    </div>
+);
+
+export default About;
---
+++ src/components/Certificates.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Certificates = ()=>{
+    <div className="Certificates">
+        <div className="Certificates-container">
+            <div className="Certificates-item">
+                <h3>MIT</h3>
+                <p>Mis certificados</p>
+            </div>
+        </div>
+    </div>
+};
+
+export default Certificates;
